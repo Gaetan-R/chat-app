27af060c9c8c73496b6783d383210f1e401ae32d
wildcodeschool <richer.gaetan33@gmail.com>
2021-01-17 22:37:47 +0100

    Firts React Project

---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+    return (
+<div className="Contact">
+    <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="random"></img>
+        <div>
+            <name>Dean Sims</name>
+
+            <div className="status">
+                <status-online></status-online>
+                <status-text>Online</status-text>
+            </div>
+        </div>
+    </div>
+
+    );
+}
+export default Contact;
